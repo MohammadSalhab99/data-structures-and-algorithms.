e1a39a0525511cc5a21317115c6aa3f5d658ebc4
MohammadSalhab99 <mohammadsalhab8@gmail.com>
2022-05-27 19:14:52 +0300

    all Done1

--- insertion-sort/tests/test_insertion_sort.py
+++ insertion-sort/tests/test_insertion_sort.py
@@ -1,5 +1,17 @@
-from insertion_sort import __version__
+from insertion_sort.insertion_sort import InsertionSort
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+
+def test_reverse_sroted():
+   actual= InsertionSort([20,18,12,8,5,-2])
+   expected = [-2,5,8,12,18,20]
+   assert actual ==expected
+
+def test_few_uniques():
+   actual= InsertionSort([5,12,7,5,5,7])
+   expected = [5,5,5,7,7,12]
+   assert actual ==expected
+def test_nearly_sroted():
+   actual= InsertionSort([2,3,5,7,13,11])
+   expected = [2,3,5,7,11,13]
+   assert actual ==expected
